85eea8539afc6fbc571d0b5b855d4665de5aff9e
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-01 15:37:27 +0530

    context toggler Light/Dark mode

--- app/components/ThemeToggleButton.js
+++ app/components/ThemeToggleButton.js
@@ -1,14 +1,17 @@
-'use client'
+'use client';
 import React from 'react';
-
+import { ThemeContext } from './ThemeProvider';
 
 const ThemeToggleButton = () => {
-    
-    return (
-       <>
-       
-       </>
-    )
+    const {theme,toggleTheme}= React.useContext(ThemeContext);
 
-}
+    return (
+       <button 
+       className={`btn btn-${theme} txt-${theme}`}
+       onClick={toggleTheme}>
+        Toggle local theme to {theme === "light" ? "dark" :"light"}
+       </button>
+    );
+
+};
 export {ThemeToggleButton}
